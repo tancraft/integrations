d33c76b93e6b34017578980421d3f451133fef43
tancraft <tancraftwork@gmail.com>
2022-02-13 14:40:42 +0100

    save all

---
+++ generetest/test.bat
@@ -0,0 +1,7 @@
+@REM @ECHO OFF
+
+@REM cd %~dp0\generetest php main.php
+
+cd %~dp0
+
+php main.php
---
+++ theme-starter/js/app.js
@@ -0,0 +1,5 @@
+const body = document.querySelector("main");
+let element = document.createElement("div");
+element.setAttribute("id", "js-id");
+element.innerHTML = "div en js";
+body.appendChild(element);
