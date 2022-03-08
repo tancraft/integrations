c0f6cc8667695a6e6e67def89aa68c06c471def2
tancraft <tancraftwork@gmail.com>
2022-03-08 06:26:07 +0100

    maj creepy

--- creepypasta/js/app.js
+++ creepypasta/js/app.js
@@ -1,21 +1,53 @@
-import Burger from'./burger.js';
-import {SocialLinks} from "./domElem.js";
+class ToggleMenu {
+  constructor(burger, navbar) {
+    this.burger = document.getElementById(burger);
+    this.navbar = document.getElementById(navbar);
+    this.navlinks = document.querySelectorAll(`#${navbar} li a`);
+    this.burger.addEventListener("click", () => {
+      this.burger.classList.toggle("active");
+      this.navbar.classList.toggle("open");
+    });
+  }
+  closLinks() {
+    this.navlinks.forEach((item) => {
+      item.addEventListener("click", () => {
+        this.burger.classList.toggle("active");
+        this.navbar.classList.toggle("open");
+      });
+    });
+  }
+}
 
-const sociaux = [
-  ["https://www.facebook.com/tancraft/", "facebook"],
-  ["https://github.com/tancraft", "github"],
-  ["https://codepen.io/tancraft", "codepen"],
-  ["https://www.deviantart.com/tancraft", "deviantart"],
-];
+
+function createImg(elt){
+  parent.innerHTML = '';
+      let url = elt.querySelector("img").src;
+      let imgTemp = document.createElement("img");
+      imgTemp.src = url;
+      parent.appendChild(imgTemp);
+}
+
+const burger = new ToggleMenu("burger", "navbar");
+const nav = document.querySelector("#menuhaut");
+
+const parent = document.querySelector(".slider figure");
+const links = document.querySelectorAll('.slider li a')
 
 window.addEventListener("DOMContentLoaded", () => {
-  const burger = new Burger("burger", "navbar");
-  const socialLinks = new SocialLinks(
-    "sociaux",
-    sociaux,
-    "sociaux",
-    "Résaux Sociaux"
-  );
+
   burger.closLinks();
-  socialLinks.createList();
+  window.addEventListener("scroll", function () {
+    window.pageYOffset < 50
+      ? nav.classList.remove("scrolled")
+      : nav.classList.add("scrolled");
+  });
+console.log(links[0])
+createImg(links[0]);
+
+  links.forEach((elt) => {
+    elt.addEventListener("click", (e) => {
+      e.preventDefault();
+      createImg(elt);
+    });
+  });
 });
