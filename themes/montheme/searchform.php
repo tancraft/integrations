a0d386dbd3be1b99ca80c7c5abe3484ddab77f94
tancraft <tancraftwork@gmail.com>
2022-07-13 01:23:30 +0200

    maj design-system

--- themes/montheme/searchform.php
+++
@@ -1,4 +0,0 @@
-<form class="search-bar" action="<?= esc_url(home_url('/'))?>">
-    <input type="text" name="s" placeholder="Recherche" value="<?= get_search_query() ?>">
-    <button type="submit">Envoyer</button>
-</form>
