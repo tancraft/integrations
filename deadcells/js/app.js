46782c3157c43c1868d3b4065719f288e6a1076e
tancraft <tancraftwork@gmail.com>
2022-11-12 07:44:50 +0100

    update

--- deadcells/js/app.js
+++ deadcells/js/app.js
@@ -1,5 +1,41 @@
-// const body = document.querySelector("main");
-// let element = document.createElement("div");
-// element.setAttribute("id", "js-id");
-// element.innerHTML = "div en js";
-// body.appendChild(element);
+class ToggleMenu {
+  constructor(burger, navbar, screenSize) {
+    this.burger =
+      document.getElementById(burger) || document.getElementById("burger");
+    this.navbar =
+      document.getElementById(navbar) || document.getElementById("navbar");;
+    this.navlinks = document.querySelectorAll(`#${navbar} li a`);
+    this.screenSize = screenSize || 800;
+  }
+
+  run() {
+    this.burger.addEventListener("click", () => {
+      this.toggleMenu();
+    });
+    this.navlinks.forEach((item) => {
+      item.addEventListener("click", () => {
+        this.toggleMenu();
+      });
+    });
+    window.addEventListener("resize", () => {
+      if (window.innerWidth > this.screenSize) {
+        this.removeMenu();
+      }
+    });
+  }
+
+  toggleMenu() {
+    this.burger.classList.toggle("active");
+    this.navbar.classList.toggle("open");
+  }
+  removeMenu() {
+    this.burger.classList.remove("active");
+    this.navbar.classList.remove("open");
+  }
+}
+
+const burger = new ToggleMenu();
+
+window.addEventListener("DOMContentLoaded", () => {
+  burger.run();
+});
--- pokecard/js/app.js
+++ pokecard/js/app.js
@@ -1,5 +1,5 @@
-const body = document.querySelector("main");
-let element = document.createElement("div");
-element.setAttribute("id", "js-id");
-element.innerHTML = "div en js";
-body.appendChild(element);
+// const body = document.querySelector("main");
+// let element = document.createElement("div");
+// element.setAttribute("id", "js-id");
+// element.innerHTML = "div en js";
+// body.appendChild(element);
